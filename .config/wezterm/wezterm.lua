598bbcfe1dc2bbabb58001fdd5cd0b0e0abaa601
andokanade <ando@jambo-inc.io>
2026-05-06 07:58:16 +0900

    update wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,8 +4,8 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.font_size = 12.0
 config.use_ime = true
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 10
+config.window_background_opacity = 0.65
+config.macos_window_background_blur = 5
 
 ----------------------------------------------------
 -- Tab
@@ -21,13 +21,13 @@ config.hide_tab_bar_if_only_one_tab = true
 
 -- タブバーの透過
 config.window_frame = {
-  inactive_titlebar_bg = "none",
-  active_titlebar_bg = "none",
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
 }
 
 -- タブバーを背景色に合わせる
 config.window_background_gradient = {
-  colors = { "#000000" },
+	colors = { "#000000" },
 }
 
 -- タブの追加ボタンを非表示
@@ -38,9 +38,9 @@ config.show_close_tab_button_in_tabs = false
 
 -- タブ同士の境界線を非表示
 config.colors = {
-  tab_bar = {
-    inactive_tab_edge = "none",
-  },
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
 }
 
 -- タブの形をカスタマイズ
@@ -50,28 +50,27 @@ local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#5c6d74"
-  local foreground = "#FFFFFF"
-  local edge_background = "none"
-  if tab.is_active then
-    background = "#ae8b2d"
-    foreground = "#FFFFFF"
-  end
-  local edge_foreground = background
-  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
-  return {
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_LEFT_ARROW },
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-    { Text = title },
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_RIGHT_ARROW },
-  }
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+	local edge_background = "none"
+	if tab.is_active then
+		background = "#ae8b2d"
+		foreground = "#FFFFFF"
+	end
+	local edge_foreground = background
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
 end)
-
 
 ----------------------------------------------------
 -- keybinds
